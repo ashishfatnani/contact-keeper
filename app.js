39459b1d986519467691cb9547a427c7da457349
ashishfatnani <ashishfatnani3@gmail.com>
2020-04-08 20:38:31 +0530

    deployment

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const port = process.env.PORT || 5000;
 const app = express();
+const path = require("path");
 connectDB();
-app.use(express.json({extended:false}));
-app.get("/",(req,res) =>
-{
-    res.json({
-        msg:"Welcome to the contact-keeper"
-    });
-});
-app.use("/api/users",require("./routes/users"));
-app.use("/api/auth",require("./routes/auth"));
-app.use("/api/contacts",require("./routes/contacts"));
+app.use(express.json({ extended: false }));
 
-app.listen(5000,() =>
-{
-    console.log("Listening to the port number 5000!");
-})
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+app.listen(port, () => {
+  console.log(`Listening to the port number ${port}`);
+});
